a9fe8631b9efca34717039e586f8fdc6ce015094
Eduardo RM <erm@Big-Boys-MacBook-Air.local>
2023-06-27 17:56:32 -0600

    Conditionals

---
+++ 13_conditionals/main.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var edad int
+	fmt.Println("Ingresa tu edad: ")
+	fmt.Scanf("%d", &edad)
+
+	if edad >= 18 {
+		fmt.Println("El usuario es mayor de edad")
+	} else {
+		fmt.Println("El usario es menor de edad")
+	}
+
+	//Multiples Condicionales (ELSE IF)
+	var calificacion int
+
+	fmt.Println("Ingresa tu calificacion:")
+	fmt.Scanf("%d", &calificacion)
+
+	if calificacion == 10 {
+		fmt.Println("Felicidades, obtuviste una calificacion perfecta")
+	} else if calificacion == 8 || calificacion == 9 {
+		fmt.Println("Aprobaste la materia")
+	} else if calificacion == 6 || calificacion == 7 {
+		fmt.Println("Aprobaste la materia, pero necesitas estudiar mas")
+	} else if calificacion >= 0 && calificacion <= 5 {
+		fmt.Println("No aprobaste la materia")
+	} else {
+		fmt.Println("Calificacion no valida")
+	}
+}
