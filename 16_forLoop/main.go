034ba425685552f95c18e96b78b1785393a2ce6c
Eduardo RM <erm@Big-Boys-MacBook-Air.local>
2023-06-29 10:06:23 -0600

    For Loop

---
+++ 16_forLoop/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	//For
+	//vatiable (declaracion); comparacion ; incrmeento o decremento
+	for i := 1; i <= 50; i++ {
+		if i%2 == 0 {
+			fmt.Println(i)
+		}
+	}
+
+}
